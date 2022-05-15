5257852ff10af2c45837c250c1fb91ae41a40991
jiwoo-jus <parkjiwoo9980@gmail.com>
2022-05-15 21:15:34 +0900

    #2 #3 1. 스케줄링 - 30분 간격으로 서울대병원 스케줄 크롤링 2. 의사 선택시 "예약 가능한 일정이 조회되지 않고 있습니다" 팝업 뜨는 것 예외처리 3. 서울대병원 이비인후과 일정 조회 (오전 -> 오전+오후)

---
+++ src/main/java/jiwjus/AllHospitalSchedules/Scheduler/SchedulerApplication.java
@@ -0,0 +1,19 @@
+package jiwjus.AllHospitalSchedules.Scheduler;
+
+import jiwjus.AllHospitalSchedules.crawler.ScheduleCrawlingSNUH;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor    // final 멤버 변수를 자동으로 생성합니다.
+@Component                  // 스프링이 필요 시 자동으로 생성하는 클래스 목록에 추가합니다.
+public class SchedulerApplication {
+
+    private final ScheduleCrawlingSNUH scheduleCrawlingSNUH;
+
+    @Scheduled(fixedRate = 1800000)     // 30분 간격으로 실행
+    public void scheduleCrawling() throws InterruptedException{
+        scheduleCrawlingSNUH.main();    // 서울대학교병원 스케줄 크롤링
+    }
+
+}
--- src/main/java/jiwjus/AllHospitalSchedules/crawler/ScheduleCrawlingSNUH.java
+++ src/main/java/jiwjus/AllHospitalSchedules/crawler/ScheduleCrawlingSNUH.java
@@ -2,115 +2,119 @@ package jiwjus.AllHospitalSchedules.crawler;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+
 //서울대학교병원
+@Component
 public class ScheduleCrawlingSNUH {
     public static void main(String... args) {
         WebDriver driver = null;
         try {
             // drvier 설정
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\parkj\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
             // Chrome 드라이버 인스턴스 설정
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+
             // 스크립트를 사용하기 위한 캐스팅
             JavascriptExecutor js = (JavascriptExecutor) driver;
+
             // 서울대학교병원 인터넷 진료예약 URL로 접속
             driver.get("https://www.snuh.org/reservation/reservation.do");
-            // 대기 설정
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);    // 대기
 
-            // 아이디 입력
-            WebElement element = driver.findElement(By.xpath("//*[@id=\"id\"]"));
+            // 로그인
+            WebElement element = driver.findElement(By.xpath("//*[@id=\"id\"]"));   // 아이디 입력칸
             element.sendKeys("bella434107");
-            // 비밀번호 입력
-            element = driver.findElement(By.xpath("//*[@id=\"pass\"]"));
+            element = driver.findElement(By.xpath("//*[@id=\"pass\"]"));            // 비밀번호 입력칸
             element.sendKeys("doby9980@@");
-            //로그인 버튼 클릭
-            element = driver.findElement(By.xpath("//*[@id=\"loginBtn\"]"));
+            element = driver.findElement(By.xpath("//*[@id=\"loginBtn\"]"));        // 로그인 버튼
             element.sendKeys(Keys.ENTER);
-            // 대기
-            Thread.sleep(3000); //1초==1000
+            Thread.sleep(1000);                                                // 대기 1초==1000
 
-            // 진료과 가나다순 조회 라디오버튼
-            element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div[1]/div/fieldset/div/label[3]"));
-            // 클릭. 사실 element.click()로도 클릭이 가능한데 가끔 호환성 에러가 발생하는 경우가 있다.
-            js.executeScript("arguments[0].click();", element);
-            // 대기
-            Thread.sleep(3000);
+            // 진료과 가나다순 조회
+            element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div[1]/div/fieldset/div/label[3]"));  //진료과 가나다순 조회 라디오버튼
+            js.executeScript("arguments[0].click();", element);                // 클릭. element.click()로도 클릭이 가능한데 가끔 호환성 에러가 발생하는 경우가 있다.
+            Thread.sleep(1000);
 
-            // 이비인후과
-            element = driver.findElement(By.xpath("//*[@id=\"feSlItem-pub3\"]/label[8]/span"));
-            // 클릭
+            // 이비인후과 조회
+            element = driver.findElement(By.xpath("//*[@id=\"feSlItem-pub3\"]/label[8]/span"));     //이비인후과
             js.executeScript("arguments[0].click();", element);
-            Thread.sleep(3000);
+            Thread.sleep(1000);
 
             // 해당 진료과 의사 목록
-            List<WebElement> elements1 = driver.findElements(By.xpath("//*[@id=\"doctorList\"]//li"));
-            Thread.sleep(3000);
+            List<WebElement> doctorElements = driver.findElements(By.xpath("//*[@id=\"doctorList\"]//li"));
+            Thread.sleep(1000);
 
             int i = 1;
             // 의사별 진료일정
-            for(WebElement element1 : elements1){
+            for(WebElement doctorElement : doctorElements){
                 System.out.println("\n doctor "+ i++);
 
-                //의사이름
-                String doctorName = element1.findElement(By.tagName("img")).getAttribute("alt"); //*[@id="doctorList"]/li[2]/div/div[2]/em[1]
+                String doctorName = doctorElement.findElement(By.tagName("img")).getAttribute("alt"); //*[@id="doctorList"]/li[2]/div/div[2]/em[1]
                 System.out.println("의사이름: " + doctorName);
-                Thread.sleep(3000);
+                Thread.sleep(1000);
 
+                //해당 의사의 예약 가능한 진료날짜 조회
                 try{
-                //선택버튼(해당 의사의 예약 가능한 진료날짜 조회)
-                WebElement element1btn = element1.findElement(By.tagName("button"));
-                element1btn.sendKeys(Keys.ENTER);
-                Thread.sleep(3000); //1초==1000
+                    WebElement doctorButtonElement = doctorElement.findElement(By.tagName("button"));                // 의사 선택버튼
+                    doctorButtonElement.sendKeys(Keys.ENTER);
+                    Thread.sleep(1000);
                 } catch (Throwable e) {     // 선택버튼이 없는 경우(전화 예약만 가능)
                     continue;
-//                    e.printStackTrace();
                 }
 
-                System.out.println(
-                        driver.findElement(By.className("ui-datepicker-year")).getText() + "년 " +
-                        driver.findElement(By.className("ui-datepicker-month")).getText() + "월"
-                );
-                Thread.sleep(3000);
+                try {
+                    System.out.println(
+                            driver.findElement(By.className("ui-datepicker-year")).getText() + "년 " +
+                            driver.findElement(By.className("ui-datepicker-month")).getText() + "월"
+                    );
+                    Thread.sleep(1000);
+                } catch(Throwable e){       // "예약 가능한 일정이 조회되고 있지 않습니다." 얼럿 창 뜨는 경우
+                    continue;
+                }
 
-                System.out.println("--오후진료");
-                List<WebElement> elements2pm = driver.findElement(By.className("calendarWrap")).findElements(By.className("schedulPM"));
-                Thread.sleep(3000);
-                int elements2pmSize = elements2pm.size();
-                System.out.println("사이즈: " + elements2pmSize);
+                List<WebElement> scheduleElements = new ArrayList<WebElement>();
+                scheduleElements.addAll(driver.findElements(By.className("schedulAM")));
+                scheduleElements.addAll(driver.findElements(By.className("schedulPM")));
+//                scheduleElements.addAll(driver.findElements(By.className("schedulAll")));
+                Thread.sleep(1000);
 
-                for(int j =0; j<elements2pmSize; j++){
+                int scheduleElementsSize = scheduleElements.size();
+                System.out.println("사이즈: " + scheduleElementsSize);
+
+                for(int j =0; j<scheduleElementsSize; j++){
                     System.out.println(j);
-                    List<WebElement> elements2pm2 = driver.findElement(By.className("calendarWrap")).findElements(By.className("schedulPM"));
-                    WebElement element2 = elements2pm2.get(j);
 
-                    Thread.sleep(3000);
-                    while (element2.isDisplayed() == false) {
-                        System.out.println("waiting..");
-                        Thread.sleep(3000);
-                    }
+                    List<WebElement> scheduleTempElements = new ArrayList<WebElement>();
+                    scheduleTempElements.addAll(driver.findElements(By.className("schedulAM")));
+                    scheduleTempElements.addAll(driver.findElements(By.className("schedulPM")));
+
+                    WebElement element2 = scheduleTempElements.get(j);
+                    Thread.sleep(1000);
 
                     System.out.println(element2.getText() + "일");
+
                     // 해당날짜의 예약 가능한 진료시간 조회
                     element2.click();
-                    Thread.sleep(3000);
+                    Thread.sleep(1000);
 
                     // 시간들
                     List<WebElement> elements3 = driver.findElement(By.xpath("//*[@class=\"time current\"]")).findElements(By.tagName("span"));
                     for (WebElement element3 : elements3) {
                         System.out.println(element3.getText());
-                        Thread.sleep(3000);
+                        Thread.sleep(1000);
                     }
                 }
-
-//                System.out.println("오전진료");
-//                List<WebElement> elements2am = driver.findElement(By.className("calendarWrap")).findElements(By.className("schedulAM"));
-//                System.out.println("종일진료");
-//                //List<WebElement> elements2all = driver.findElement(By.className("calendarWrap")).findElements(By.className("schedulAll"));
             }
         } catch (Throwable e){
             e.printStackTrace();
